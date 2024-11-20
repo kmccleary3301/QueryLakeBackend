aa340e1f918faac5afe7c9cb639b0577829ffffd
kmccleary3301 <kmccl24>
2024-11-20 14:41:40 -0600

    Patching up collection ownership changes

--- QueryLake/api/collections.py
+++ QueryLake/api/collections.py
@@ -169,34 +169,35 @@ def fetch_all_collections(database : Session,
 
 def fetch_collection(database : Session,
                      auth : AuthType,
-                     collection_hash_id : str,
-                     collection_type : str = "user"):
+                     collection_hash_id : str):
     """
     Retrieves details of a collection for user, 
     including all documents in the collection.
     """
 
-    assert collection_type in ["user", "organization", "global"], "Invalid collection type"
     (user, user_auth) = get_user(database, auth)
     collection = database.exec(select(sql_db_tables.document_collection).where(and_(sql_db_tables.document_collection.id == collection_hash_id))).first()
     assert not collection is None, "Collection not found"
     
-    if collection_type == "user":
+    if collection.collection_type in ["user", "toolchain_session"]:
         if collection.public == False:
             assert collection.author_user_name == user_auth.username, "User not authorized to view collection"
         
         owner = "personal"
-    else:
+    elif collection.collection_type == "organization":
         user_membership = database.exec(select(sql_db_tables.organization_membership).where(and_(sql_db_tables.organization_membership.organization_id == collection.author_organization,
                                                                                           sql_db_tables.organization_membership.user_name == user_auth.username))).all()
         assert len(user_membership) > 0 or collection.public == True, "User not authorized to view collection"
         author_org = database.exec(select(sql_db_tables.organization).where(sql_db_tables.organization.id == collection.author_organization)).first()
-        owner = author_org.name()
+        assert not author_org is None, f"Author organization `{collection.author_organization}` not found"
+        owner = author_org.name
+    elif collection.collection_type == "global":
+        owner = "global"
 
     data = {
         "title" : collection.name,
         "description": collection.description,
-        "type": collection_type,
+        "type": collection.collection_type,
         "owner": owner,
         "public": collection.public,
         "document_count": collection.document_count
@@ -347,8 +348,8 @@ def assert_collections_priviledge(database : Session,
         memberships : List[sql_db_tables.organization_membership] = list(database.exec(
             select(sql_db_tables.organization_membership)
             .where(and_(sql_db_tables.organization_membership.organization_id.in_(organization_ids),
-                    sql_db_tables.organization_membership.user_name == auth.username))
-        ).first())
+                    sql_db_tables.organization_membership.user_name == user_auth.username))
+        ).all())
         
         membership_lookup = {x.organization_id: x for x in memberships}
         viewables = {
@@ -479,7 +480,13 @@ def get_collection_document_password(
     collection = database.exec(select(sql_db_tables.document_collection).where(sql_db_tables.document_collection.id == collection_id)).first()
     assert not collection is None, "Collection not found"
     
-    if collection.collection_type == "user":
+    if collection.collection_type == "global" or collection.public == True:
+        
+        # Encryption key stored plainly for global and public collections.
+        encryption_key = collection.encryption_key_secure
+        document_password = encryption.aes_decrypt_string(encryption_key, collection.document_unlock_key)
+    
+    elif collection.collection_type == "user":
         
         assert collection.author_user_name == user_auth.username, "User not authorized"
         private_key_encryption_salt = user.private_key_encryption_salt
@@ -508,7 +515,7 @@ def get_collection_document_password(
         encryption_key_secured = collection.encryption_key_secure
         encryption_key = encryption.ecc_decrypt_string(organization_private_key, encryption_key_secured)
         document_password = encryption.aes_decrypt_string(encryption_key, collection.document_unlock_key)
-        
+    
     elif collection.collection_type == "toolchain_session":
         # private_key_encryption_salt = user.private_key_encryption_salt
         # user_private_key_decryption_key = hash_function(user_auth.password_prehash, private_key_encryption_salt, only_salt=True)
@@ -522,6 +529,140 @@ def get_collection_document_password(
     
     return document_password
     
+
+def change_collection_ownership(
+    database : Session, 
+    auth: AuthType,
+    collection_id: str,
+    organization_id: str = None,
+    user_name: str = None,
+    global_collection: bool = False,
+    public: bool = None
+):
+    """
+    Change the ownership of a collection.
+    Could be organization, user, or global (only if user is an admin).
+    """
     
+    (user, user_auth) = get_user(database, auth)
     
+    assert isinstance(global_collection, bool), "Invalid global collection value, must be boolean"
+    
+    data_val = [
+        not organization_id is None,
+        not user_name is None,
+        global_collection
+    ]
+    
+    assert sum([1 if e else 0 for e in data_val]) == 1, "Must provide exactly one of organization_id, user_name, or global_collection as True."
+    
+    collection = database.exec(
+        select(sql_db_tables.document_collection)
+        .where(sql_db_tables.document_collection.id == collection_id)
+    ).first()
+    
+    assert not collection is None, "Collection not found"
+    
+    if public is None:
+        public = collection.public
+    
+    assert isinstance(public, bool), "Invalid public value, must be boolean"
+    
+    if collection.collection_type == "organization":
+        organization = database.exec(
+            select(sql_db_tables.organization)
+            .where(sql_db_tables.organization.id == collection.author_organization)
+        ).first()
+        membership = database.exec(
+            select(sql_db_tables.organization_membership)
+            .where(and_(
+                sql_db_tables.organization_membership.organization_id == organization.id,
+                sql_db_tables.organization_membership.user_name == user_auth.username,
+                sql_db_tables.organization_membership.invite_still_open == False
+            ))
+        ).first()
+        assert not membership is None, "User not authorized"
+        assert membership.role in ["owner", "admin"], "User not authorized, must be owner or admin to change collection ownership."  
+        print("First switch met condition `org`")
+        
+    elif collection.collection_type == "user":
+        assert collection.author_user_name == user_auth.username, "User not authorized"
+        print("First switch met condition `user`")
+    elif collection.collection_type == "global":
+        assert user.is_admin, "User not authorized"
+        print("First switch met condition `global`")
+    else:
+        print("First switch met condition `exit`")
+        raise ValueError(f"Collection type `{collection.collection_type}` not supported on this method.")
+    
+    document_password_unencrypted = get_collection_document_password(database, auth, collection_id)
+    
+    new_collection_type = "organization" if organization_id else "user" if user_name else "global"
+    
+    if new_collection_type == "global" or public:
+        new_encryption_key = random_hash(base=16)
+        doc_password_secured = encryption.aes_encrypt_string(new_encryption_key, document_password_unencrypted)
+        
+        collection.encryption_key_secure = new_encryption_key
+        collection.document_unlock_key = doc_password_secured
+        print("Second switch met condition `global or switch`")
+    elif new_collection_type == "user":
+        new_encryption_key = random_hash(base=16)
+        new_encryption_key_secured = encryption.ecc_encrypt_string(user.public_key, new_encryption_key)
+        doc_password_secured = encryption.aes_encrypt_string(new_encryption_key, document_password_unencrypted)
+        
+        collection.encryption_key_secure = new_encryption_key_secured
+        collection.document_unlock_key = doc_password_secured
+        print("Second switch met condition `user`")
+        
+    if new_collection_type in ["global", "user"]:
+        collection.author_user_name = user.name
+        collection.author_organization = None
+        
+        print("Third switch met condition `global or user`")
+    elif new_collection_type == "organization":
+        
+        organization_new = database.exec(
+            select(sql_db_tables.organization)
+            .where(sql_db_tables.organization.id == organization_id)
+        ).first()
+        assert not organization_new is None, "Organization not found"
+        
+        membership_new = database.exec(
+            select(sql_db_tables.organization_membership)
+            .where(and_(
+                sql_db_tables.organization_membership.organization_id == organization_id,
+                sql_db_tables.organization_membership.user_name == user_auth.username,
+                sql_db_tables.organization_membership.invite_still_open == False
+            ))
+        ).first()
+        assert not membership_new is None, "User not authorized"
+        assert membership_new.role in ["owner", "admin"], \
+            "User not authorized, must be owner or admin to add an organization collection."  
+
+        
+        collection.author_organization = organization_new.id
+        collection.author_user_name = None
+        print("Third switch met condition `org`")
+        
+        if not public:
+            new_encryption_key = random_hash(base=16)
+            new_encryption_key_secured = encryption.ecc_encrypt_string(organization_new.public_key, new_encryption_key)
+            doc_password_secured = encryption.aes_encrypt_string(new_encryption_key, document_password_unencrypted)
+            
+            collection.encryption_key_secure = new_encryption_key_secured
+            collection.document_unlock_key = doc_password_secured
+            print("Third switch met condition `org - private`")
+    
+    collection.collection_type = new_collection_type
+    collection.public = public
+    
+    print("Collection ownership changed:", type(collection), collection)
+    
+    database.commit()
+    
+    return_dict = {k: v for k, v in collection.model_dump().items() if not k in ["document_unlock_key"]}
+    
+    return return_dict
+        
     
