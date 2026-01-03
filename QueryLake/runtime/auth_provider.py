2301413fc2da2f9fcc879e17ae266a0cfb51418a
kmccleary3301 <kmccl24@lsu.edu>
2026-01-02 19:12:06 -0600

    Add auth provider registry skeleton

---
+++ QueryLake/runtime/auth_provider.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Dict
+
+
+class AuthProvider:
+    def issue_token(self, principal: dict, scopes: list[str]) -> str:
+        raise NotImplementedError
+
+    def validate_token(self, token: str) -> dict:
+        raise NotImplementedError
+
+    def refresh_token(self, token: str) -> str:
+        raise NotImplementedError
+
+
+AUTH_PROVIDERS: Dict[str, AuthProvider] = {}
+
+
+def register_provider(name: str, provider: AuthProvider) -> None:
+    AUTH_PROVIDERS[name] = provider
+
